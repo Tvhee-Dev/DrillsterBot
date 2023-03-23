5fc1b8920a871a3c424681162906c2454a468e67
Tijn van Heesewijk <tvhee100@gmail.com>
2023-03-23 17:59:34 +0100

    Reformat

--- src/drillster.py
+++ src/drillster.py
@@ -1,7 +1,14 @@
+# Drill thread manager - manages all the current Drillster threads and updates the progress bar
+import os
+import threading
+import time
 import requests
 import json
 
-header: json
+header = {}
+current_drills = []
+thread_lock: threading.Lock
+wordlists = {}
 
 
 def set_token(drillster_token):
@@ -11,50 +18,154 @@ def set_token(drillster_token):
 
 
 def get_repertoire():
-    return requests.get("https://www.drillster.com/api/3/repertoire",
-                        headers=header).json()["playableRenditions"]
+    return requests.get("https://www.drillster.com/api/3/repertoire", headers=header).json()["playableRenditions"]
 
 
 def get_course_content(playable):
+    link = f"https://www.drillster.com/api/3/results?playable={playable}"
+
     try:
-        return requests.get(f"https://www.drillster.com/api/3/results?playable={playable}",
-                            headers=header).json()["results"]
-    except:
-        return requests.get(f"https://www.drillster.com/api/3/results?playable={playable}",
-                            headers=header).json()["results"]
+        return requests.get(link, headers=header).json()["results"]
+    except EOFError:
+        return requests.get(link, headers=header).json()["results"]
+
+
+def start_drills(drill_ids):
+    # Load the whole wordlist file
+    global wordlists
+    global current_drills
+    global thread_lock
+
+    if os.path.exists("wordlists.json"):
+        with open("wordlists.json", "r") as file_content:
+
+            file_read = file_content.read()
+
+            if file_read != "":
+                wordlists = json.loads(file_read)
+
+    # Create threads for each drill and start them
+
+    thread_lock = threading.Lock()
+
+    for drill_id in drill_ids:
+        drill = Drill(drill_id)
+        current_drills.append(drill)
+
+    # Update the percentage bar every second
+    percentage = 0
+
+    while percentage < 100:
+        percentage = update_progressbar()
+        time.sleep(1)
+
+    # Reset the current_drills list
+    current_drills = []
+
+
+def update_progressbar():
+    drill_amount = len(current_drills)
+    percentage = 0
+    completed = 0
+
+    for drill in current_drills:
+        # part / whole (for the correct calculation the start percentage has been removed
+        # and this percentage is added to the total percentage with 1 / drill_amount
+
+        # If the Drill is finished then add it as whole
+        if drill.percentage == 100 or drill.start_percentage == 100:
+            completed += 1
+            percentage += 100
+        else:
+            # delta_percentage / total_percentage added for 1 / drill_amount drills
+            delta_percentage = drill.percentage - drill.start_percentage
+            total_percentage = 100 - drill.start_percentage
+            drill_percentage = (delta_percentage / total_percentage) * 100
+            percentage += drill_percentage
+
+    # By dividing at the end of the calculation, the calculation is more accurate
+    percentage = percentage / drill_amount
+    progressbar_percent = max([percentage, 0])
+    characters_filled = round(progressbar_percent / 2)
+    characters_empty = round((100 - progressbar_percent) / 2)
+
+    print(f"[{characters_filled * '=' + characters_empty * ' '}] {round(percentage)}% ({completed} / {drill_amount} Drills completed)", end="\r")
+
+    return percentage
 
 
 class Drill:
     start_percentage = -1
     percentage = -1
+    reference = None
 
     def __init__(self, drill_id):
-        self.reference = None
         self.__dict__ = requests.get(f"https://www.drillster.com/api/2.1.1/playable/{drill_id}", headers=header).json()
+        thread = threading.Thread(target=self.start_drill)
+        thread.start()
 
-    def get_name(self):
-        return self.name
+    def start_drill(self):
+        # Load existing dictionary of questions and answers from file if it exists, otherwise create empty dictionary
+        wordlist = wordlists[self.id] if self.id in wordlists else {}
+
+        while self.percentage < 100:
+            # Get a question from the drill
+            question_object = self.get_question()
+            question = question_object["ask"]["term"]["value"]
+            answer_object: json
+
+            # Check if the question is already in the dictionary of questions and answers
+            if question not in wordlist:
+                # Answer the question and add the question-answer pair to the dictionary
+                answer_object = self.answer_question(answer="")
+            else:
+                # Answer the question using the previously recorded answer from the dictionary
+                answer_object = self.answer_question(answer=wordlist[question])
+
+            # First set the answer, after setting check if the answer is correct - Drills could change
+            if answer_object["evaluation"] == "INCORRECT":
+                # Get all the possible answers
+                correct_answers = answer_object["evaluation"]["termEvaluations"]
+
+                # Check for multiple answers - store them all
+                if question_object["tell"]["composition"] != "SET":
+                    # Store the only correct answer
+                    wordlist[question] = correct_answers[1]["value"]
+
+                else:
+                    correct_answers = []
+
+                    # Store all possible answers
+                    for answer in correct_answers:
+                        if answer["value"] != "":
+                            correct_answers.append(answer["value"])
+
+                    wordlist[question] = correct_answers
 
     def get_question(self):
         question = requests.get(f"https://www.drillster.com/api/2.1.1/question/{self.id}", headers=header).json()
         question_object = question["question"]
         self.reference = question_object["reference"]
+
         return question_object
 
     def answer_question(self, answer):
+        answer_response: json
+        send_question_data: json
+
         if isinstance(answer, str):
-            answer_response = requests.put(f"https://www.drillster.com/api/2.1.1/answer/{self.reference}",
-                                        headers=header, data={"answer": answer}).json()
-        elif isinstance(answer, list):
-            data = []
-            for i in answer:
-                data.append(("answer",i))
-            answer_response = requests.put(f"https://www.drillster.com/api/2.1.1/answer/{self.reference}",
-                                        headers=header, data=data).json()
+            send_question_data = {"answer": answer}
+        else:
+            send_question_data = []
+
+            for index in answer:
+                send_question_data.append(("answer", index))
+
+        answer_response = requests.put(f"https://www.drillster.com/api/2.1.1/answer/{self.reference}",
+                                       headers=header, data=send_question_data).json()
         self.percentage = answer_response["proficiency"]["overall"]
+
         if self.start_percentage == -1:
             self.start_percentage = self.percentage
-        return answer_response
 
-    def continue_answering(self):
-        return self.percentage < 100
+        return answer_response
--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
-import sys
-import threading
+# Front end loader - user has to select the Drills / Courses here
+import time
+
 import inquirer
-import os
-import json
 import drillster
 import browser_cookie3
-
-current_drills = []
 
 
 def start():
@@ -31,10 +28,14 @@ def start():
         print("No token found! Please login to Drillster on any webbrowser and keep the tab open!")
         exit()
 
-    start_drills(select_drills())
+    start_time = time.time()
+    drillster.start_drills(select_drills())
+    delta_time = round(time.time() - start_time)
+
+    print("")
+    print(f"DrillsterBot has finished the selected Drills in {delta_time} seconds!")
 
 
-# Show menu to select courses/drills
 def select_drills():
     repertoire = drillster.get_repertoire()
     playables = [(index["name"], index["id"]) for index in repertoire]
@@ -61,10 +62,13 @@ def select_drills():
             print("Playing all Drills in this course!")
             # Gets all ids from tuple list
             return [drill[1] for drill in drill_list]
+
         else:
             return selected_drills
+
     elif [item["type"] for item in repertoire if item["id"] == play_id][0] == "DRILL":
         return [play_id]
+
     else:
         print("Cannot play tests using DrillsterBot")
         exit()
@@ -90,112 +94,5 @@ def extract_playable_drills(repertoire_list):
 
     return result
 
-def get_wordlist(drill_id):
-    if drill_id in wordlists:
-        return wordlists[drill_id]
-    else:
-        return {}
 
-def store_wordlist(drill_id, wordlist_data):
-    wordlists[drill_id] = wordlist_data
-    with open("wordlists.json", "w") as file_content:
-        json.dump(wordlists, file_content)
-
-# Define a function to play a drill given its ID
-def start_drill(drill_id):
-    #proficiency = 0
-    #start_time = time.time()
-    drill = drillster.Drill(drill_id)
-    current_drills.append(drill)
-
-    # Load existing dictionary of questions and answers from a file if it exists, otherwise create an empty dictionary
-    global wordlists
-    wordlists={}
-    if os.path.exists("wordlists.json"):
-        with open("wordlists.json", "r") as file_content:
-            file_read=file_content.read()
-            if file_read != "":
-                
-                wordlists=json.loads(file_read)
-
-    stored_wordlist = get_wordlist(drill_id)
-    while drill.continue_answering():
-        # Get a question from the drill
-        question_object = drill.get_question()
-        question = question_object["ask"]["term"]["value"]
-        answer_object: json
-
-        # Check if the question is already in the dictionary of questions and answers
-        if question not in stored_wordlist:
-            # Answer the question and add the question-answer pair to the dictionary
-            answer_object = drill.answer_question(answer="")
-            if question_object["tell"]["composition"] != "SET":
-                stored_wordlist[question_object["ask"]["term"]["value"]] = \
-                    answer_object["evaluation"]["termEvaluations"][1]["value"]
-            elif question_object["tell"]["composition"] == "SET":
-                correct_answers=[]
-                for ans in answer_object["evaluation"]["termEvaluations"]:
-                    if ans["value"] != '':
-                        correct_answers.append(ans["value"])
-                stored_wordlist[question_object["ask"]["term"]["value"]] = \
-                    correct_answers
-        else:
-            # Answer the question using the previously recorded answer from the dictionary
-            answer_object = drill.answer_question(answer=stored_wordlist[question])
-    	
-        lock.acquire()
-        update_progressbar()
-        lock.release()
-    # Save the updated dictionary of questions and answers to a file
-    store_wordlist(drill_id,stored_wordlist)
-
-    #lock.acquire()
-    # Print a message indicating that the drill is completed and how long it took to complete
-    #print(f"Completed {drill.get_name()} in {round(time.time() - start_time,1)} seconds")
-    #lock.release()
-
-
-def start_drills(drill_ids):
-    # Create threads for each drill and start them
-    threads = []
-    global lock 
-    lock = threading.Lock()
-    for drill_id in drill_ids:
-        thread = threading.Thread(target=start_drill, args=(drill_id,))
-        threads.append(thread)
-        thread.start()
-
-    #print(f"Progress: [                    ] 0% (0 / {len(drill_ids)} Drills completed)")
-
-    # Wait for all threads to finish before continuing
-    for thread in threads:
-        thread.join()
-
-    global current_drills
-    current_drills = []
-
-    print("")
-    print("DrillsterBot has finished!")
-
-
-def update_progressbar():
-    global current_drills
-    drill_amount = len(current_drills)
-    percentage = 0
-    completed = 0
-
-    for drill in current_drills:
-        # part / whole (for the correct calculation the start percentage has been removed
-        # and this percentage is added to the total percentage with 1 / drill_amount
-
-        # !!! vvv This crashes if start_percentage = 100 vvv !!!
-        if drill.start_percentage != 100:
-            percentage += (((drill.percentage - drill.start_percentage) / (100 - drill.start_percentage))*100 / drill_amount)
-        else:
-            percentage += 100/drill_amount
-
-        if drill.percentage == 100:
-            completed += 1
-    progressbar_percent=max([percentage,0])
-    print(f"[{round(progressbar_percent/2)*'='+round((100-progressbar_percent)/2)*' '}] {round(percentage)}% ({completed} / {drill_amount} Drills completed)", end="\r")
 start()
