6becfabcacd51922d31e6787f9cf7f935e767ea6
Tijn van Heesewijk <tvhee100@gmail.com>
2023-05-14 22:17:10 +0200

    Fixed Chrome cookie storage location

--- src/main/java/me/tvhee/drillsterbot/cookie/CookieManager.java
+++ src/main/java/me/tvhee/drillsterbot/cookie/CookieManager.java
@@ -12,6 +12,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,43 +26,42 @@ import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public final class CookieManager
 {
     private final File cookieStoreCopy;
+    private final Map<File, String> cookieLocations; //Storage, Decryption key
     private String chromeKeyringPassword = null;
     
     public CookieManager()
     {
-        cookieStoreCopy = new File(System.getProperty("java.io.tmpdir"), "cookies.store");
+        this.cookieStoreCopy = new File(System.getProperty("java.io.tmpdir"), "cookies.store");
+        this.cookieLocations = getCookieLocations();
     }
     
     public Set<Cookie> getCookies()
     {
-        HashSet<Cookie> cookies = new HashSet<>();
-        
-        for(File cookieStore : getCookieStores())
-            cookies.addAll(processCookies(cookieStore, null));
-        
-        return cookies;
+        return getCookiesForDomain(null);
     }
     
     public Set<Cookie> getCookiesForDomain(String domain)
     {
         Set<Cookie> cookies = new HashSet<>();
         
-        for(File cookieStore : getCookieStores())
-            cookies.addAll(processCookies(cookieStore, domain));
+        for(Map.Entry<File, String> cookieStore : cookieLocations.entrySet())
+            cookies.addAll(processCookies(cookieStore.getKey(), cookieStore.getValue(), domain));
         
         return cookies;
     }
     
-    private Set<Cookie> processCookies(File cookieStore, String domainFilter)
+    private Set<Cookie> processCookies(File cookieStore, String masterKey, String domainFilter)
     {
-        boolean chrome = !cookieStore.getAbsolutePath().contains("Mozilla");
-        HashSet<Cookie> cookies = new HashSet<>();
+        boolean chrome = masterKey != null;
+        Set<Cookie> cookies = new HashSet<>();
         
         if(!cookieStore.exists())
             return cookies;
@@ -71,65 +71,6 @@ public final class CookieManager
             cookieStoreCopy.delete();
             Files.copy(cookieStore.toPath(), cookieStoreCopy.toPath());
             
-            /*if(cookieStore.getAbsolutePath().endsWith(".jsonlz4"))
-            {
-                byte[] fileData = Files.readAllBytes(cookieStore.toPath());
-                byte[] compressedData = Arrays.copyOfRange(fileData, 8, fileData.length);
-                LZ4Factory factory = LZ4Factory.fastestInstance();
-                LZ4FastDecompressor decompressor = factory.fastDecompressor();
-                
-                int decompressedLength = ByteBuffer.wrap(fileData, 0, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
-                int blockSize = 2048 * 4096; // 8 megabyte
-                byte[] block = new byte[blockSize];
-                int remaining = decompressedLength;
-                
-                try(InputStream inputStream = new BufferedInputStream(new LZ4BlockInputStream(new ByteArrayInputStream(compressedData), decompressor)))
-                {
-                    PipedOutputStream pipedOutputStream = new PipedOutputStream();
-                    
-                    try(OutputStream outputStream = new BufferedOutputStream(pipedOutputStream))
-                    {
-                        while(remaining > 0)
-                        {
-                            int bytesToRead = Math.min(remaining, blockSize);
-                            int bytesRead = inputStream.read(block, 0, bytesToRead);
-                            
-                            if(bytesRead == -1)
-                                throw new EOFException("Unexpected end of input");
-                            
-                            outputStream.write(block, 0, bytesRead);
-                            remaining -= bytesRead;
-                        }
-                    }
-                    
-                    // create a BufferedReader from the PipedInputStream
-                    PipedInputStream pipedInputStream = new PipedInputStream(pipedOutputStream);
-                    
-                    try(BufferedReader reader = new BufferedReader(new InputStreamReader(pipedInputStream)))
-                    {
-                        JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
-                        
-                        for(JsonElement cookieElement : json.get("cookies").getAsJsonArray())
-                        {
-                            JsonObject cookie = cookieElement.getAsJsonObject();
-                            String name = cookie.get("name").getAsString();
-                            String data = cookie.get("value").getAsString();
-                            String path = cookie.get("path").getAsString();
-                            String domain = cookie.get("host").getAsString();
-                            boolean secure = cookie.get("secure").getAsBoolean();
-                            boolean httpOnly = cookie.get("httponly").getAsBoolean();
-                            Date expires = new Date(cookie.get("expiry").getAsLong());
-                            
-                            if(!domain.contains(domainFilter))
-                                continue;
-                            
-                            cookies.add(new Cookie(name, data.getBytes(), false, expires, path, domain, secure, httpOnly, cookieStore));
-                        }
-                    }
-                }
-            }
-            else*/
-            
             // load the sqlite-JDBC driver using the current class loader
             Class.forName("org.sqlite.JDBC");
             // create a database connection
@@ -137,13 +78,13 @@ public final class CookieManager
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30); // set timeout to 30 seconds
             ResultSet result;
+            String databaseName = chrome ? "cookies" : "moz_cookies";
+            String hostRow = chrome ? "host_key" : "host";
             
             if(domainFilter == null || domainFilter.isEmpty())
-                result = statement.executeQuery("select * from " + (chrome ? "cookies" : "moz_cookies"));
+                result = statement.executeQuery("select * from " + databaseName);
             else
-                result = statement.executeQuery(
-                        "select * from " + (chrome ? "cookies" : "moz_cookies") + " where " + (chrome ? "host_key" : "host") + " like \"%" + domainFilter
-                                + "%\"");
+                result = statement.executeQuery("select * from " + databaseName + " where " + hostRow + " like \"%" + domainFilter + "%\"");
             
             while(result.next())
             {
@@ -154,21 +95,12 @@ public final class CookieManager
                 boolean secure = hasColumn(result, chrome ? "secure" : "ISSECURE") && result.getBoolean(chrome ? "secure" : "ISSECURE");
                 boolean httpOnly = chrome && hasColumn(result, "httponly") && result.getBoolean("httponly");
                 Date expires = result.getDate(chrome ? "expires_utc" : "EXPIRY");
-                byte[] decryptedData = null;
+                byte[] data = cookieData; //Firefox cookies are already decrypted
                 
-                for(File localState : getLocalStateFiles())
-                {
-                    if(!localState.exists())
-                        continue;
-                    
-                    decryptedData = decryptChromeCookie(cookieData, localState);
-                    
-                    if(decryptedData != null)
-                        break;
-                }
+                if(masterKey != null)
+                    data = decryptChromiumCookie(cookieData, masterKey);
                 
-                cookies.add(new Cookie(name, decryptedData != null ? decryptedData : cookieData, decryptedData != null, expires, path, domain, secure, httpOnly,
-                        cookieStore));
+                cookies.add(new Cookie(name, data, data != cookieData || !chrome, expires, path, domain, secure, httpOnly, cookieStore));
             }
             
             cookieStoreCopy.delete();
@@ -177,19 +109,19 @@ public final class CookieManager
         catch(Exception e)
         {
             e.printStackTrace();
-            // if the error message is "out of memory",
-            // it probably means no database file is found
+            //If the error message is "out of memory", it probably means no database file is found
         }
         
         return cookies;
     }
     
-    private byte[] decryptChromeCookie(byte[] encryptedCookie, File pathLocalState)
+    private byte[] decryptChromiumCookie(byte[] encryptedCookie, String encryptedMasterKeyWithPrefixB64)
     {
         byte[] decryptedCookie = null;
         boolean windows = System.getProperty("os.name").toLowerCase().contains("win");
         boolean mac = System.getProperty("os.name").toLowerCase().contains("mac");
-        boolean linux = System.getProperty("os.name").toLowerCase().contains("nix") || System.getProperty("os.name").toLowerCase().contains("nux")
+        boolean linux = System.getProperty("os.name").toLowerCase().contains("nix")
+                || System.getProperty("os.name").toLowerCase().contains("nux")
                 || System.getProperty("os.name").toLowerCase().indexOf("aix") > 0;
         
         if(windows)
@@ -204,25 +136,19 @@ public final class CookieManager
             {
                 try
                 {
-                    if(!pathLocalState.exists())
-                        return null;
-                    
-                    // Get encrypted master key
-                    JsonObject jsonObjectLocalState = JsonParser.parseReader(new FileReader(pathLocalState)).getAsJsonObject();
-                    String encryptedMasterKeyWithPrefixB64 = jsonObjectLocalState.get("os_crypt").getAsJsonObject().get("encrypted_key").getAsString();
-                    
                     // Remove prefix (DPAPI)
                     byte[] encryptedMasterKeyWithPrefix = Base64.getDecoder().decode(encryptedMasterKeyWithPrefixB64);
                     byte[] encryptedMasterKey = Arrays.copyOfRange(encryptedMasterKeyWithPrefix, 5, encryptedMasterKeyWithPrefix.length);
                     // Decrypt
                     byte[] masterKey = Crypt32Util.cryptUnprotectData(encryptedMasterKey);
-                    // Separate praefix (v10), nonce and ciphertext/tag
+                    // Separate prefix (v10), nonce and ciphertext/tag
                     byte[] nonce = Arrays.copyOfRange(encryptedCookie, 3, 3 + 12);
                     byte[] ciphertextTag = Arrays.copyOfRange(encryptedCookie, 3 + 12, encryptedCookie.length);
                     // Decrypt
                     Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
                     GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128, nonce);
                     SecretKeySpec keySpec = new SecretKeySpec(masterKey, "AES");
+                    
                     cipher.init(Cipher.DECRYPT_MODE, keySpec, gcmParameterSpec);
                     decryptedCookie = cipher.doFinal(ciphertextTag);
                 }
@@ -286,51 +212,92 @@ public final class CookieManager
         return decryptedCookie;
     }
     
-    private Set<File> getLocalStateFiles()
+    private Map<File, String> getCookieLocations()
     {
-        Set<File> masterKeyLocations = new HashSet<>();
-        masterKeyLocations.add(new File(System.getProperty("user.home") + "\\AppData\\Local\\Google\\Chrome\\User Data\\Local State"));
-        masterKeyLocations.add(new File(System.getProperty("user.home") + "\\AppData\\Local\\Microsoft\\Edge\\User Data\\Local State"));
-        masterKeyLocations.add(new File(System.getProperty("user.home") + "/Library/Application Support/Google/Chrome/User Data/Local State")); //Mac
-        masterKeyLocations.add(new File(System.getProperty("user.home") + "/.config/chromium/User Data/Local State")); //Linux
-        return masterKeyLocations;
-    }
-    
-    private Set<File> getCookieStores()
-    {
-        Set<File> cookieStores = new HashSet<>();
+        String userHome = System.getProperty("user.home");
+        Map<File, String> files = new HashMap<>();
         
-        cookieStores.add(new File(System.getProperty("user.home") + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Network\\Cookies"));
-        cookieStores.add(new File(System.getProperty("user.home") + "\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Network\\Cookies"));
-        cookieStores.add(new File(System.getProperty("user.home") + "/Library/Application Support/Google/Chrome/Default/Network/Cookies")); //Mac
-        cookieStores.add(new File(System.getProperty("user.home") + "/.config/chromium/Default/Network/Cookies")); //Linux
-        
-        //Firefox
-        for(String path : new String[]{ "\\Application Data\\Mozilla\\Firefox\\Profiles\\", "\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\",
-                "/Library/Application Support/Firefox/Profiles/", "/.config/Firefox/", "/.config/firefox/", "/.mozilla/firefox/" })
+        try
         {
-            File profilesDirectory = new File(System.getProperty("user.home") + path);
+            //Windows - Chrome
+            File windowsChromeLocalState = new File(userHome + "\\AppData\\Local\\Google\\Chrome\\User Data\\Local State");
             
-            if(profilesDirectory.exists())
+            if(windowsChromeLocalState.exists())
             {
-                for(File file : profilesDirectory.listFiles())
+                JsonObject jsonObjectLocalState = JsonParser.parseReader(new FileReader(windowsChromeLocalState)).getAsJsonObject();
+                String lastUsedProfile = jsonObjectLocalState.get("profile").getAsJsonObject().get("last_used").getAsString();
+                
+                File windowsChromeCookieData = new File(userHome + "\\AppData\\Local\\Google\\Chrome\\User Data\\" + lastUsedProfile + "\\Network\\Cookies");
+                
+                if(windowsChromeCookieData.exists())
+                    files.put(windowsChromeCookieData, getEncryptionKey(windowsChromeLocalState));
+            }
+            
+            //Windows - Edge
+            File windowsEdgeCookieData = new File(userHome + "\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Network\\Cookies");
+            File windowsEdgeLocalState = new File(userHome + "\\AppData\\Local\\Microsoft\\Edge\\User Data\\Local State");
+            
+            if(windowsEdgeLocalState.exists() && windowsEdgeCookieData.exists())
+                files.put(windowsEdgeCookieData, getEncryptionKey(windowsChromeLocalState));
+            
+            //Mac - Chrome
+            File macChromeCookieData = new File(userHome + "/Library/Application Support/Google/Chrome/Default/Network/Cookies");
+            File macChromeLocalState = new File(userHome + "/Library/Application Support/Google/Chrome/User Data/Local State");
+            
+            if(macChromeCookieData.exists())
+                files.put(macChromeCookieData, getEncryptionKey(macChromeLocalState));
+            
+            //Linux - Chrome
+            File linuxChromeCookieData = new File(userHome + "/.config/chromium/Default/Network/Cookies");
+            File linuxChromeLocalState = new File(userHome + "/.config/chromium/User Data/Local State");
+            
+            if(linuxChromeCookieData.exists())
+                files.put(linuxChromeCookieData, getEncryptionKey(linuxChromeLocalState));
+            
+            //General - Firefox
+            for(String path : new String[]{ "\\Application Data\\Mozilla\\Firefox\\Profiles\\", "\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\",
+                    "/Library/Application Support/Firefox/Profiles/", "/.config/Firefox/", "/.config/firefox/", "/.mozilla/firefox/" })
+            {
+                File profilesDirectory = new File(userHome + path);
+                
+                if(profilesDirectory.exists())
                 {
-                    if(file.isDirectory() && file.getName().matches("(?iu)^.*?\\.default-release$"))
+                    File[] listFiles = profilesDirectory.listFiles();
+                    File[] directoryFiles = listFiles == null ? new File[]{} : listFiles;
+                    
+                    for(File file : directoryFiles)
                     {
-                        File cookieDatabase = new File(System.getProperty("user.home") + path + file.getName() + File.separator + "cookies.sqlite");
-                        //File recoveryFile = new File(System.getProperty("user.home") + path + file.getName() + File.separator + "sessionstore-backups" + File.separator + "recovery.jsonlz4");
-                        
-                        if(cookieDatabase.exists())
-                            cookieStores.add(cookieDatabase);
-                        
-                        //if(recoveryFile.exists())
-                        //cookieStores.add(recoveryFile);
+                        if(file.isDirectory() && file.getName().matches("(?iu)^.*?\\.default-release$"))
+                        {
+                            File cookieDatabase = new File(userHome + path + file.getName() + File.separator + "cookies.sqlite");
+                            
+                            if(cookieDatabase.exists())
+                                files.put(cookieDatabase, null);
+                        }
                     }
                 }
             }
         }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
         
-        return cookieStores;
+        return files;
+    }
+    
+    private String getEncryptionKey(File localState)
+    {
+        try
+        {
+            JsonObject jsonObjectLocalState = JsonParser.parseReader(new FileReader(localState)).getAsJsonObject();
+            return jsonObjectLocalState.get("os_crypt").getAsJsonObject().get("encrypted_key").getAsString();
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
     }
     
     private boolean hasColumn(ResultSet resultSet, String columnName) throws SQLException
